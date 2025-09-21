ec2f31175ab12ad461c961e5840cd2e6e8957f05
“Narahari <abhinavnarahari@gmail.com>
2025-09-21 15:40:41 +0530

    Updated folder structure

--- app/onboarding.tsx
+++
@@ -1,7 +0,0 @@
-// app/onboarding.tsx
-import React from 'react';
-import OnboardingScreen from '../screens/OnboardingScreen';
-
-export default function OnboardingRoute() {
-  return <OnboardingScreen />;
-}
